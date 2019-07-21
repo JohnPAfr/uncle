8f2888f96c67de88a8b38cf98e37d42bf72e9384
JohnPAfr <john.aurry@gmail.com>
2019-07-21 21:12:15 +0200

    first commit, html is ok, need to implement photos with JS, css to improve, just the basic with grid and flex

---
+++ galerie.js
@@ -0,0 +1,7 @@
+const galerie = document.querySelector('.galerie');
+
+let photo = document.createElement('div');
+
+photo.appendChild(document.createElement('h1'));    
+
+galerie.appendChild(photo);
