f745f9b7df643358fbc6df2c1e3220d652984a52
JohnPAfr <john.aurry@gmail.com>
2019-07-26 10:21:47 +0200

    file containing variables

---
+++ variables.js
@@ -0,0 +1,50 @@
+let photos = [
+    {
+        "name": "toto",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "tata",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "toto",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "tata",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "toto",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "tata",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "toto",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "tata",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "toto",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "tata",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "toto",
+        "src": "https://picsum.photos/300"
+    },
+    {
+        "name": "tata",
+        "src": "https://picsum.photos/300"
+    }
+]
